17f334e8ecb0c08bb11ddadfd6d6961d69465e88
saru <sarulathaannadurai@gmail.com>
2021-02-15 10:51:18 +0530

    Added features

--- script2.js
+++ script2.js
@@ -1,8 +1,27 @@
 let life = sessionStorage.getItem("life");
 let result = document.getElementById("result"); 
+let myMusic = new AddSound("Audio/Guitar-Mayhem-3.mp3")
+myMusic.play();
+myMusic.loop = true;
+
 if(life>0){  
-    result.innerHTML = `Hurray!! You Won!! Aliens are restored back.Score : ${life}`
+    result.innerHTML = `Hurray!! You Won!! Score : ${life}`
 }else{
-    result.innerHTML = `Hurray!! Aliens are restored back.Score : ${life}`
     result.innerHTML = "Game Over !!"
+}
+
+
+function AddSound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+    this.stop = function () {
+        this.sound.pause();
+    }
 }
